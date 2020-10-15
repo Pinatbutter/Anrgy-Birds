28d9f3971ce7f69e13a2af7409303dc832cae0f4
Maverick1037 <70665291+Maverick1037@users.noreply.github.com>
2020-10-15 15:32:35 -0500

    Fixed the Menu
    Menu was running multiple errors that I cleaned. Made a new script called MenuCamera.

---
+++ Assets/Scripts/MenuCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuCamera : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public static int level;    // The current level
+
+    public void SwitchLevel(int lvl)
+    {
+        level = lvl;
+        SceneManager.LoadScene("GameScene");
+    }
+}
