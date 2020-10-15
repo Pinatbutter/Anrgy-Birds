bcbed101136f9847f4c91d2e2833215e6b456957
Maverick1037 <70665291+Maverick1037@users.noreply.github.com>
2020-10-15 16:04:25 -0500

    Menu works completely now

--- Assets/Scripts/MenuCamera.cs
+++ Assets/Scripts/MenuCamera.cs
@@ -5,13 +5,10 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuCamera : MonoBehaviour
-{
-    [Header("Set Dynamically")]
-    public static int level;    // The current level
-
+{ 
     public void SwitchLevel(int lvl)
     {
-        level = lvl;
         SceneManager.LoadScene("GameScene");
+        MissionDemolition.level = lvl;
     }
 }
