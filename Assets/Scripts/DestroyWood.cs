a5c149d6487cc2fb1bbc76d1643aac6aad7e856b
Maverick1037 <70665291+Maverick1037@users.noreply.github.com>
2020-10-19 21:10:53 -0500

    Castle work, Break Blocks
    Straightened up some castles

---
+++ Assets/Scripts/DestroyWood.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWood : MonoBehaviour
+{
+
+    public int hitPoints = 1;
+
+    private bool broken = false;
+
+    public GameObject woodPiece1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hitPoints == 0)
+        {
+            Destroy(woodPiece1);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 velocity = collision.relativeVelocity;
+        if (velocity.magnitude > 10)
+        {
+            if (hitPoints > 0)
+            {
+                hitPoints--;
+            }
+
+        }
+
+    }
+}
